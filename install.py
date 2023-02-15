3ee646caf250e38e1edc56882818b15ebc00791a
khalilrached <khalilrached058@gmail.com>
2023-02-15 01:08:09 +0100

    package installer

---
+++ install.py
@@ -0,0 +1,20 @@
+import os
+import pip
+import platform
+
+
+package_list = ["tensorflow","pygame"]
+
+PYTHON_PATH = ""
+
+for path in os.get_exec_path():
+    if path.lower().find("python") != -1:
+        PYTHON_PATH = path
+print(PYTHON_PATH)
+
+if platform.system().lower() == "windows":
+    for package in package_list:
+        os.system(PYTHON_PATH + f"\\python.exe -m pip install {package}")
+else:
+    for package in package_list:
+        os.system(PYTHON_PATH+f"/python -m pip {package}")
