20292c83e33746100679e5fc2e515d2e021d62c1
khalilrached <khalilrached058@gmail.com>
2023-02-16 23:43:37 +0100

    player detected collision with an object

--- lib/player.py
+++ lib/player.py
@@ -18,6 +18,24 @@ class Player:
     def draw(self):
         pygame.draw.rect(self.display, self.__COLOR, self.player_rect)
 
+    def isCollided(self, _object):
+        sky, ground, x_collide, y_top_collide, y_bottom_collide = False, False, False, False, False
+        if _object.boundaries()["x-boundaries"][0] <= self.player_rect.x + self.player_rect.w <= \
+                _object.boundaries()["x-boundaries"][1]:
+            x_collide = True
+        # upper pillar
+        if 0 <= self.player_rect.y <= _object.boundaries()["y-boundaries"][0]:
+            y_top_collide = True
+        # lower pillar
+        if _object.boundaries()["y-boundaries"][1] <= self.player_rect.y + self.player_rect.h <= self.display.get_height():
+            y_bottom_collide = True
+        # hit sky
+        if self.player_rect.y <= 0:
+            sky = True
+        if self.player_rect.y + self.player_rect.h >= self.display.get_height():
+            ground = True
+        return x_collide and (y_bottom_collide or y_top_collide) or sky or ground
+
     def boundaries(self):
         return {
             "x-boundaries": [self.player_rect.x, self.player_rect.x + self.player_rect.w],
