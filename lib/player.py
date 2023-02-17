90b54f8757ea28b47fc73cc022eb982522546f40
khalilrached <khalilrached058@gmail.com>
2023-02-17 04:48:52 +0100

    adding delay to jump method / adding inputs method

--- lib/player.py
+++ lib/player.py
@@ -1,22 +1,34 @@
+import time
+
 import pygame
 from lib.physics import *
 
 
 class Player:
-    __SIZE = 30
+    SIZE = 30
     __COLOR = (255, 0, 0)
-    jump_force = -500
-
+    jump_force = -300
+    isJumping = False
     def __init__(self, display, start_pos_x, start_pos_y):
         self.display = display
-        self.player_rect = pygame.rect.Rect(start_pos_x, start_pos_y, self.__SIZE, self.__SIZE)
-        self.gravity = GravityForce(1000, self.player_rect)
+        self.player_rect = pygame.rect.Rect(start_pos_x, start_pos_y, self.SIZE, self.SIZE)
+        self.gravity = GravityForce(1200, self.player_rect)
 
     def jump(self):
-        self.gravity.add_force(self.jump_force)
+        if not self.isJumping:
+            self.isJumping = True
+            self.gravity.add_force(self.jump_force)
+        else:
+            self.isJumping = False
 
     def draw(self):
         pygame.draw.rect(self.display, self.__COLOR, self.player_rect)
+
+    def inputs(self, _object):
+        player_position_y = self.player_rect.y + (self.player_rect.h / 2)
+        player_position_x = self.player_rect.x + (self.player_rect.w / 2)
+        distance = abs(_object.boundaries()["x-boundaries"][0] - player_position_x)
+        return player_position_y, _object.boundaries()["y-boundaries"][0], _object.boundaries()["y-boundaries"][1], distance
 
     def isCollided(self, _object):
         sky, ground, x_collide, y_top_collide, y_bottom_collide = False, False, False, False, False
